49823f4be7414c6c00ccd0e8f7a8a5ab16824955
Shyam Johnson <tjshyam0@gmail.com>
2026-03-24 07:21:42 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,22 @@
 import mysql.connector
 import os
 
+
 def get_connection():
-    connection = mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME", "campus_lost_found"),
-        port=int(os.getenv("DB_PORT", 3306))
-    )
-    return connection
+    try:
+        print("Connecting to DB...")
+
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT", 3306))
+        )
+
+        print("DB Connected ✅")
+        return connection
+
+    except Exception as e:
+        print("DB ERROR ❌:", e)
+        raise
